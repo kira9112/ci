0c720e4d6497d8808b3d3bf5f99cb94f0ae7fdb2
Lamborz <geniusessay@gmail.com>
2021-07-12 18:47:10 +0500

    first test msg

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
-
+//test
   @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
