b62b1fc59d6f7b655ca38cd4e14d40fbe3f73792
Kira9112 <kira91_911@mail.ru>
2021-07-14 00:36:14 +0300

    adeded readmi.md

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,6 +1,6 @@
 package ru.netology.statistic;
 
-public class StatisticsService {
+public class StatisticsService<incomes> {
   /**
    * Calculate index of max income
    *
@@ -9,11 +9,21 @@ public class StatisticsService {
    */
   public long findMax(long[] incomes) {
     long current_max_index= 0;
-    long current_max = incomes[0];
+    long current_max = incomes[1];
     for (long income : incomes)
       if (current_max < income)
         current_max = income;
         return current_max;
+
+  }
+
+  public long findMin(long[] incomes) {
+    long current_min_index= 0;
+    long current_min = incomes[0];
+    for (long income : incomes)
+      if (current_min > income)
+        current_min = income;
+    return current_min;
 
   }
 }
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -8,10 +8,21 @@ class StatisticsServiceTest {
   @Test
   void findMax() {
     StatisticsService service = new StatisticsService();
-    long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
 
     long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void findMin() {
+    StatisticsService service = new StatisticsService();
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 3;
+
+    long actual = service.findMin(incomesInBillions);
 
     assertEquals(expected, actual);
 
